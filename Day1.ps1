055495a8603de7e09d930ea5546d338d22978b03
Andrew White <andrew@fundapps.co>
2020-12-01 16:56:47 +0000

    Update Day1.ps1

--- Day1.ps1
+++ Day1.ps1
@@ -1,15 +1,40 @@
-$lines = ([IO.File]::ReadAllLines("C:\Temp\day1.txt"))
-for($i = 0; $i -lt $lines.Count; $i++)
-{
-    $x = $lines[$i] -as [int]
-    for($j = $i + 1; $j -lt $lines.Count; $j++)
+function Part1 {
+    $lines = ([IO.File]::ReadAllLines("C:\Temp\day1.txt"))
+    for($i = 0; $i -lt $lines.Count; $i++)
     {
-        $y = $lines[$j] -as [int]
-        if ($x + $y -eq 2020)
+        $x = $lines[$i] -as [int]
+        for($j = $i + 1; $j -lt $lines.Count; $j++)
         {
-            $result = $x * $y
-            Write-Host $result
-            break;
+            $y = $lines[$j] -as [int]
+            if ($x + $y -eq 2020)
+            {
+                $result = $x * $y
+                Write-Host $result
+                break;
+            }
         }
     }
 }
+function Part2 {
+    $lines = ([IO.File]::ReadAllLines("C:\Temp\day1.txt"))
+    for($i = 0; $i -lt $lines.Count; $i++)
+    {
+        $x = $lines[$i] -as [int]
+        for($j = $i + 1; $j -lt $lines.Count; $j++)
+        {
+            $y = $lines[$j] -as [int]
+            for($k = $j + 1; $k -lt $lines.Count; $k++)
+            {
+                $z = $lines[$k] -as [int]
+                if ($x + $y + $z -eq 2020)
+                {
+                    $result = $x * $y * $z
+                    Write-Host $result
+                    break;
+                }
+            }
+        }
+    }
+}
+Part1
+Part2
