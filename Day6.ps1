8b5f6db77529662dfe85c52687247df53a23bb86
Andrew White <andrew@fundapps.co>
2020-12-06 11:15:15 +0000

    6

---
+++ Day6.ps1
@@ -0,0 +1,25 @@
+function Part1($text) {
+    $groupCount = 0
+    foreach($group in $text.Split("`n`n")) {
+        $answers = @{}
+        $group.Replace("`n", "").ToCharArray() | ForEach-Object { $answers[$_]++ }
+        $groupCount += $answers.Count
+    }
+    return $groupCount
+}
+function Part2($text) {
+    $groupCount = 0
+    foreach($group in $text.Split("`n`n")) {
+        $answerCounts = @{}
+        $people = $group.Split("`n")
+        foreach($personAnswers in $people) {
+            $personAnswers.ToCharArray() | ForEach-Object { $answerCounts[$_]++ }
+        }
+        $groupCount += $answerCounts.GetEnumerator().Where({$_.Value -eq $people.Count}).Count
+    }
+    return $groupCount
+}
+$text = ([IO.File]::ReadAllText("C:\Dev\Advent2020\Day6Input.txt"))
+$result1 = Part1 $text
+$result2 = Part2 $text
+Write-Host $result1 $result2
